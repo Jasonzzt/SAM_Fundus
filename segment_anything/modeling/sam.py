9ecfb3d6238d9c9b8b08c99f1dc5ddd0330132ac
Dear_Tony <105782374+Tonylmz@users.noreply.github.com>
2023-05-15 11:21:16 +0800

    Update sam.py

--- segment_anything/modeling/sam.py
+++ segment_anything/modeling/sam.py
@@ -50,7 +50,7 @@ class Sam(nn.Module):
     def device(self) -> Any:
         return self.pixel_mean.device
 
-    @torch.no_grad()
+#     @torch.no_grad()
     def forward(
         self,
         batched_input: List[Dict[str, Any]],
@@ -95,7 +95,8 @@ class Sam(nn.Module):
                 to subsequent iterations of prediction.
         """
         input_images = torch.stack([self.preprocess(x["image"]) for x in batched_input], dim=0)
-        image_embeddings = self.image_encoder(input_images)
+        with torch.no_grad():
+            image_embeddings = self.image_encoder(input_images)
 
         outputs = []
         for image_record, curr_embedding in zip(batched_input, image_embeddings):
